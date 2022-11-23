d271883588fc95a3f67a5779b07cfd9a9f657547
Jim Mack <jim.mack@upstart.com>
2022-11-23 00:52:42 -0800

    setting up version right

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,22 +2,37 @@ package version
 
 import (
 	"fmt"
-
-	"github.com/spf13/cobra"
-
-	v "github.com/jmacksf/dd-cli/internal/version"
+	"runtime"
+	"strconv"
+	"time"
 )
 
-// NewCmdVersion is a version command.
-func NewCmdVersion() *cobra.Command {
-	return &cobra.Command{
-		Use:   "version",
-		Short: "Print the app version information",
-		Long:  "Print the app version and build information",
-		Run:   version,
-	}
-}
+// Build information is populated at build-time.
+var (
+	Version         = "v0.0.0-dev"
+	GitCommit       = ""
+	SourceDateEpoch = "-1"
+	GoVersion       = runtime.Version()
+	Compiler        = runtime.Compiler
+	Platform        = fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)
+)
 
-func version(*cobra.Command, []string) {
-	fmt.Println(v.Info())
+// Info returns version and build information.
+func Info() string {
+	i, err := strconv.ParseInt(SourceDateEpoch, 10, 64) //nolint:gomnd
+	if err != nil {
+		panic(err)
+	}
+	commitDate := ""
+	if i >= 0 {
+		// https://pkg.go.dev/time#Time.Format
+		//
+		//     $ TZ=MST date -Iseconds -d"Jan 2 15:04:05 2006 MST"
+		//     2006-01-02T15:04:05-07:00
+		commitDate = time.Unix(i, 0).UTC().Format("2006-01-02T15:04:05-07:00")
+	}
+	return fmt.Sprintf(
+		"(Version=\"%s\", GitCommit=\"%s\", CommitDate=\"%s\", GoVersion=\"%s\", Compiler=\"%s\", Platform=\"%s\")",
+		Version, GitCommit, commitDate, GoVersion, Compiler, Platform,
+	)
 }
